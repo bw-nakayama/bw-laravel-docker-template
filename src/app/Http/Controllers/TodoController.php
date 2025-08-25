b3a9744df4b370af595f634f3462021ae63cd13b
Toru Nakayama <nakayamatohru.b@gmail.com>
2025-08-26 08:39:36 +0900

    feat:todosテーブルをマッピング

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Todo;
 
 class TodoController extends Controller
 {
     public function index()
     {
+        $todo = new Todo();
         //dd('Hello World!');
+        $todos = $todo->all();
+
+        dd($todos);
+
         return view('todo.index');
     }
 }
