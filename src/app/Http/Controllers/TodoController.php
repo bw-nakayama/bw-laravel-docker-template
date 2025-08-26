9be22c8208e902a6732f1ba0f08a4a7719b77755
Toru Nakayama <nakayamatohru.b@gmail.com>
2025-08-26 10:52:50 +0900

    feat:新規作成処理の

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -24,13 +24,10 @@ class TodoController extends Controller
 
     public function store(Request $request)
     {
-        $content = $request->input('content');
+        $inputs = $request->all();
 
-        // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
-        $todo = new Todo(); 
-        // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
-        $todo->content = $content;
-        // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
+        $todo = new Todo();
+        $todo->fill($inputs); // 変更
         $todo->save();
 
         return redirect()->route('todo.index');
